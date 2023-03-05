fa682c0906d0d4c6fb3c66999db6a64f90da3cdf
radio-satellites <114111180+radio-satellites@users.noreply.github.com>
2023-03-05 11:38:58 -0500

    cyclic buffer fix

--- parser/main.py
+++ parser/main.py
@@ -3,6 +3,7 @@ import time
 import sys
 from datetime import datetime
 import parser
+import collections
 
 _s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 _s.settimeout(1)
@@ -10,20 +11,33 @@ _s.connect(("127.0.0.1",7322))
 
 outputfile = open("log.txt",'w')
 
-message = ""
+d = collections.deque(maxlen=42)
+
+def update_cyclic_buffer(string):
+    d.append(string)
+    return 0
+
+def return_cyclic_buffer():
+    return list(d)
+
+def return_cyclic_buffer_string():
+    return ''.join(return_cyclic_buffer())
+def flush_buffer():
+    d = collections.deque(maxlen=42)
 
 while True:
     try:
          _char = _s.recv(1).decode()
          #print(_char)
-         message += _char
+         update_cyclic_buffer(_char) #Update cyclic parsing buffer
          #print(message)       
-         if _char == "\n" or _char == "1":
-            try:
-                print(parser.parse(message))
-            except:
-                print("CANNOT PARSE MESSAGE :(")
-            message = ""
+         try:
+            print(return_cyclic_buffer_string())
+            print(parser.parse_string(return_cyclic_buffer_string()))
+         except Exception as e:
+            print(e)
+            print("CANNOT PARSE MESSAGE :(")
+
                 
              
              
