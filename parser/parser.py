54be319c2f6ca0b50e1d0e2285eb494848cfce12
radio-satellites <114111180+radio-satellites@users.noreply.github.com>
2023-03-12 13:31:42 -0400

    Update parser.py

--- parser/parser.py
+++ parser/parser.py
@@ -1,27 +1,40 @@
-#example_string = "$$,99900,12100,2300,43669380,79521368" #test string
+
 
 def parse_string(string):
     parameters = string.split(",")
+    num_correct = 0
     try:
         pressure = float(parameters[1]) #Recover the data back
+        num_correct = num_correct+1
     except:
         pressure = 0
     try:
         altitude = float(parameters[2])
+        num_correct = num_correct+1
     except:
         altitude = 0
     try:
         temperature = float(parameters[3])
+        num_correct = num_correct+1
     except:
         temperature = 0
     try:
         latitude = float(parameters[4])/1000000
+        if latitude > 20 and latitude < 100:
+            num_correct = num_correct+1
+            
     except:
         latitude = 0
     try:
         longitude = float(parameters[5])/-1000000
+        if longitude < -10 and longitude > -100:
+            num_correct = num_correct+1
     except:
         longitude = 0
+    if num_correct > 0:
+        return pressure,altitude,temperature,latitude,longitude
+    else:
+        return 0
         
-    return pressure,altitude,temperature,latitude,longitude,finished
+    
 
