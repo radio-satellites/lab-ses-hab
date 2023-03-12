2e1cd4d1438874f6143de1f4471e8ee241462098
radio-satellites <114111180+radio-satellites@users.noreply.github.com>
2023-03-12 13:32:21 -0400

    Update main.py

--- parser/main.py
+++ parser/main.py
@@ -7,8 +7,9 @@ import collections
 
 _s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 _s.settimeout(1)
+num_nochars = 0
 try:
-_s.connect(("127.0.0.1",7322))
+    _s.connect(("127.0.0.1",7322))
 except:
     print("ERROR: COULD NOT CONNECT TO FLDIGI")
 
@@ -17,12 +18,14 @@ packet = ""
 while True:
     try:
          _char = _s.recv(1).decode()
+         num_nochars = 0
          #print(_char)
          packet = packet + _char #Update buffer
          #print(message)
          if _char == "\n":
              try:
                 print(parser.parse_string(packet))
+                packet = ""
              except Exception as e:
                 print(e)
                 print("ERROR: CANNOT PARSE MESSAGE :(")
@@ -32,5 +35,7 @@ while True:
              
              
     except:
-        print("WARN: NO CHARS RECEIVED...")
+        if num_nochars >= 5:
+            print("WARN: NO CHARS RECEIVED...")
+            num_nochars = num_nochars+1
         pass
